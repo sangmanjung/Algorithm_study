52d78b6de584d1641d7525bed420dfcd80ba8107
Sangman Jung <sangmanjung@khu.ac.kr>
2020-12-30 02:03:41 +0900

    Add files via upload

---
+++ basic/S6_exhaustive_search/sec6-4.py
@@ -0,0 +1,32 @@
+import sys
+#sys.stdin = open('input.txt','rt')
+
+def DFS(v):
+    global cnt
+    if v == N+1:
+        tmp1, tmp2 = [], []
+        for i in range(1,N+1):
+            if check[i] == 1:
+                tmp1.append(elem[i-1])
+            else:
+                tmp2.append(elem[i-1])
+        if sum(tmp1) == sum(tmp2):
+            cnt += 1
+
+    else:
+        check[v] = 1
+        DFS(v+1)
+        check[v] = 0
+        DFS(v+1)
+
+    return cnt
+
+if __name__ == '__main__':
+    N = int(input())
+    elem = list(map(int,input().split()))
+    check = [0]*(N+1)
+    cnt = 0
+    if DFS(1) != 0:
+        print('YES')
+    else:
+        print('NO')
